2bc6f8d2c5df9ca0fdb6e6c88850d5db06f28494
juanmarcos <acevedojuan17@gmail.com>
2022-10-28 17:51:32 -0500

    Creando contenido en el navbar

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import NavBar from './components/NavBar/NavBar';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <NavBar />
-      </header>
     </div>
   );
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,7 +2,15 @@ import './style.css'
 
 const NavBar = () => {
   return (
-    <nav class="prueba">Hello, World!</nav>
+    <nav class="nav-bar">
+      <a class="nav-bar__item">Home</a>
+      <a class="nav-bar__item">Novedades</a>
+      <a class="nav-bar__item">Más vendidos</a>
+      <a class="nav-bar__item">Artistas</a>
+      <a class="nav-bar__item">Géneros</a>
+      <a class="nav-bar__item">Nosotros</a>
+      <a class="nav-bar__item">Contacto</a>
+    </nav>
   )
 }
 
