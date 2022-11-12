9abf82d6e296bc6e4b1258c663a17dffe09b2f63
juanmarcos <acevedojuan17@gmail.com>
2022-11-12 15:50:25 -0500

    Creando archivos de componente ItemCount + prueba de renderizado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import NavBar from './components/NavBar/NavBar';
 import ItemsListContainer from './components/ItemsListContainer/ItemsListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
         <NavBar />
         <ItemsListContainer greeting="Bienvenido a roygbiv record store" message="Acá encontrarás tus álbumes favoritos, en un formato atemporal"/>
+        <ItemCount />
     </div>
   );
 }
