eff894c78ff4920b8f169dcb6fc7ecfd1c22d5f8
juanmarcos <acevedojuan17@gmail.com>
2022-11-12 17:39:32 -0500

    Agregando estilos a casilla de agregar ítems al carrito

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,26 +1,31 @@
 import { useState } from 'react';
+import './ItemCount.css'
 
 const ItemCount = () => {
   const [count, setCount] = useState(0);
   let stk = 10;
-  const addCart = () => {
+  const addItem = () => {
     if (count < stk) {
       setCount(count + 1)
     }
   };
-  const removeCart = () => {
+  const removeItem = () => {
     if (count > 0) {
       setCount(count - 1);
     }
   }
+  
   return (
-    <div>
-      <div>Agregar a carrito</div>
-      <div>{count}</div>
-      <div>
-        <button onClick={removeCart}>-</button>
-        <button onClick={addCart}>+</button>
+    <div className='main-container'>
+          <div className='item-count__container'>
+      <div className='item-count__container__counter'>
+        <div className='item-count__container__counter__quantity'>Cantidad:</div>
+        <button onClick={removeItem} className='item-count__container__counter__add-btn'>-</button>
+        <div className='item-count__container__counter__number'>{count}</div>
+        <button onClick={addItem} className='item-count__container__counter__remove-btn'>+</button>
       </div>
+      <button className='item-count__container__add-cart'>Agregar a carrito</button>
+    </div>
     </div>
   )
 }
