0842d013539886ca9264d4e9de00c19c6548d751
juanmarcos <acevedojuan17@gmail.com>
2022-11-12 16:45:08 -0500

    Incluyendo condición de stock

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -2,15 +2,24 @@ import { useState } from 'react';
 
 const ItemCount = () => {
   const [count, setCount] = useState(0);
-  const addCart = () => setCount(count + 1);
-  const removeCart = () => setCount(count - 1);
+  let stk = 10;
+  const addCart = () => {
+    if (count < stk) {
+      setCount(count + 1)
+    }
+  };
+  const removeCart = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  }
   return (
     <div>
       <div>Agregar a carrito</div>
       <div>{count}</div>
       <div>
-        <button onClick={addCart}>+</button>
         <button onClick={removeCart}>-</button>
+        <button onClick={addCart}>+</button>
       </div>
     </div>
   )
