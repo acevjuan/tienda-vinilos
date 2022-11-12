35887841f743d48d90924cdd2cf039ad43d9f59e
juanmarcos <acevedojuan17@gmail.com>
2022-11-12 16:02:42 -0500

    Funcionamiento básico de contador

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,6 +1,18 @@
+import { useState } from 'react';
+
 const ItemCount = () => {
+  const [count, setCount] = useState(0);
+  const addCart = () => setCount(count + 1);
+  const removeCart = () => setCount(count - 1);
   return (
-    <div>ItemCount</div>
+    <div>
+      <div>Agregar a carrito</div>
+      <div>{count}</div>
+      <div>
+        <button onClick={addCart}>+</button>
+        <button onClick={removeCart}>-</button>
+      </div>
+    </div>
   )
 }
 
