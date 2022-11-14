ada778d2d1df8607a476c0f83520f4f81ec4b134
juanmarcos <acevedojuan17@gmail.com>
2022-11-14 16:00:43 -0500

    Estilado de componente Item

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -3,11 +3,14 @@ import { NavLink } from 'react-router-dom'
 
 const Item = ( {album} ) => {
   return (
-    <NavLink to={`/item/${album.id}`}>
-      <div>
-        <img alt={album.title} src={album.cover} style={{maxHeight: '200px'}}/>
-        <h2>{album.title}</h2>
-        <h3>{album.artist}</h3>
+    <NavLink to={`/item/${album.id}`} className='main-container'>
+      <div className='item__cover'>
+        <img className='item__cover__img' alt={album.title} src={album.cover} />
+      </div>
+      <div className='item__info'>
+        <h2 className='item__info__title'>{album.title}</h2>
+        <h3 className='item__info__artist'>{album.artist}</h3>
+        <h4 className='item__info__genre'>{album.genre}</h4>
       </div>
     </NavLink>
   )
