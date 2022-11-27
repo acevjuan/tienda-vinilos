ade49888c7bd8b0f1570b82ce11ee2c7ee25f974
juanmarcos <acevedojuan17@gmail.com>
2022-11-27 13:21:51 -0500

    Creando routing para carrito (componente Cart)

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom';
 import cart from '../../assets/cart.png'
 import './style.css'
 
 const CartWidget = () => {
   return (
-    <div className="nav-bar__cart">
+    <NavLink to='/cart' className="nav-bar__cart">
       <img className="nav-bar__cart__img" src={cart} alt="carrito de compras"></img>
-    </div>
+    </NavLink>
   )
 }
 
