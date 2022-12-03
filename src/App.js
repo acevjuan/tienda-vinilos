36264dfb3bd232d3a7dc6f0a777e1eb73fa7b7b2
juanmarcos <acevedojuan17@gmail.com>
2022-12-03 17:10:49 -0500

    Creación de componente CartProvider e inclusión en App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CartProvider from './context/CartContext';
 import NavBar from './components/NavBar/NavBar';
 import ItemsListContainer from './components/ItemsListContainer/ItemsListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
@@ -7,15 +8,17 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<ItemsListContainer greeting="Bienvenido a roygbiv record store" message="Acá encontrarás tus álbumes favoritos, en un formato atemporal"/>} />
-        <Route path='/category/:filterBy' element={<ItemsListContainer greeting="Bienvenido a roygbiv record store" message="Acá encontrarás tus álbumes favoritos, en un formato atemporal"/>} />
-        <Route path='/item/:albumId' element={<ItemDetailContainer />} />
-        <Route path='/cart' element={<Cart />} />
-      </Routes>
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<ItemsListContainer greeting="Bienvenido a roygbiv record store" message="Acá encontrarás tus álbumes favoritos, en un formato atemporal"/>} />
+          <Route path='/category/:filterBy' element={<ItemsListContainer greeting="Bienvenido a roygbiv record store" message="Acá encontrarás tus álbumes favoritos, en un formato atemporal"/>} />
+          <Route path='/item/:albumId' element={<ItemDetailContainer />} />
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
   );
 }
 
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,3 +1,20 @@
 import { createContext } from "react";
+import { useState } from "react";
 
-export const CartContext = createContext();
+export const CartContext = createContext([]);
+
+const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const addItem = (item, quantity) => {
+    setCart([...cart, { item, quantity }]);
+  }
+  
+  return(
+    <CartContext.Provider value={{ cart, addItem }}>
+      {children}
+    </CartContext.Provider>
+  )
+};
+
+export default CartProvider;
