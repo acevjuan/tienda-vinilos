b27ff68596c4709a9140c44d5756dc40edb835de
juanmarcos <acevedojuan17@gmail.com>
2022-12-04 12:59:42 -0500

    Agregando renderizado condicional a carrito

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,20 +4,26 @@ import './Cart.css';
 
 const Cart = () => {
   const { cart, clearCart } = useContext(CartContext);
-  return (
-    <>
-      {cart.map((cart) => (
-        <div key={cart.id}>
-          <img src={cart.cover} alt={cart.title} style={{ maxWidth: '75px' }} />
-          <h3>{cart.title}</h3>
-          <h4>{cart.artist}</h4>
-          <h5>{cart.quantity}</h5>
-          <h5>$ {cart.price * cart.quantity}</h5>
-        </div>
-      ))}
-      <button onClick={clearCart}>Vaciar carrito</button>
-    </>
-  )
+  
+  if(!cart.length == 0) {
+    return (
+      <>
+        {cart.map((cart) => (
+          <div key={cart.id}>
+            <img src={cart.cover} alt={cart.title} style={{ maxWidth: '75px' }} />
+            <h3>{cart.title}</h3>
+            <h4>{cart.artist}</h4>
+            <h5>{cart.quantity}</h5>
+            <h5>$ {cart.price * cart.quantity}</h5>
+          </div>
+        ))}
+        <button onClick={clearCart}>Vaciar carrito</button>
+      </>
+    )
+  } else {
+    return(<h2>Carrito vacío</h2>)
+  }
+  
 }
 
 export default Cart
