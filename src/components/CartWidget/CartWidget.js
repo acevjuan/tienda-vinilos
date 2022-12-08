1a6e7d4446342c960c3d313def0e01d8e0b13f9a
juanmarcos <acevedojuan17@gmail.com>
2022-12-08 12:26:56 -0500

    Mostrando en CartWidget el número total de items agregados al carrito

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -5,14 +5,14 @@ import cartIcon from '../../assets/cart-icon.png'
 import './style.css'
 
 const CartWidget = () => {
-  const { cart } = useContext(CartContext);
+  const { cart, totalAdded } = useContext(CartContext);
   
   return (
     <>
       <NavLink to='/cart' className="nav-bar__cart">
         <img className="nav-bar__cart__img" src={cartIcon} alt="carrito de compras" />
       </NavLink>
-      <div className={cart.length === 0 ? 'nav-bar__item-count__none' : 'nav-bar__item-count'}>{cart.length}</div>
+      <div className={cart.length === 0 ? 'nav-bar__item-count__none' : 'nav-bar__item-count'}>{totalAdded}</div>
     </>
   )
 }
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,10 +1,17 @@
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 import { useState } from "react";
 
 export const CartContext = createContext([]);
 
 const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
+
+  const [totalAdded, setTotalAdded] = useState(0);
+
+  const getItemsTotals = () => {
+    setTotalAdded(cart.reduce((acc, product) => acc + product.quantity, 0));
+    console.log('testing...')
+  }
 
   const isInCart = (id) => {
     return cart.some((album) => album.id === id);
@@ -22,8 +29,12 @@ const CartProvider = ({ children }) => {
     setCart([]);
   };
   
+  useEffect(() => {
+    getItemsTotals();
+  }, [cart])
+
   return(
-    <CartContext.Provider value={{ cart, addToCart, clearCart }}>
+    <CartContext.Provider value={{ cart, totalAdded, addToCart, clearCart }}>
       {children}
     </CartContext.Provider>
   )
